49f90ae555174ca9bafcc8f1b51d3f244043896d
Volodymyr Kocherhan <volodymyrkocherhan@gmail.com>
2021-09-16 21:27:35 +0300

    done

---
+++ index.js
@@ -0,0 +1,15 @@
+const lights = ['red', 'yellow', 'green', 'yellow'];
+
+function trafficLights() {
+  const previousLight = lights.shift(); // поверт видалю світло
+  lights.push(previousLight);
+  console.log(previousLight);
+  // return previousLight;
+}
+
+let timerId = setInterval(trafficLights, 1000);
+
+setTimeout(() => {
+  clearInterval(timerId);
+  alert('stop');
+}, 39000);
