92b622ecf5b60dc04ada415cd71f7a8e49cbbd6e
Volodymyr Kocherhan <volodymyrkocherhan@gmail.com>
2021-09-17 23:21:26 +0300

    doneVersion2

--- index2.js
+++ index2.js
@@ -1,7 +1,12 @@
 const lights = ['red', 'yellow', 'green', 'yellow'];
-let curent = '';
-for (let i = 0; i < lights.length; i++) {
-  // console.log(lights[i]);
-  alert(lights[i]);
-  console.log(lights[i]);
-}
+
+const IntervalId = setInterval(() => {
+  for (let i = 0; i < lights.length; i++) {
+    console.log(lights[i]);
+    alert(lights[i]);
+  }
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(IntervalId);
+}, 9000);
